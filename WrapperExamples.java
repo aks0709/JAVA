1fee0829d04012af6f9890f344602b3ac702fdd0
aks0709 <111643861+aks0709@users.noreply.github.com>
2024-03-12 01:54:28 +0530

    Add files via upload

---
+++ WrapperExamples.java
@@ -0,0 +1,11 @@
+public class WrapperExamples
+{
+    public static void main(String[] args) {
+        int a=14;
+        int b=2;
+        Integer x=24;
+        Integer y=new Integer(56);
+        System.out.println(y);
+    }
+
+}
